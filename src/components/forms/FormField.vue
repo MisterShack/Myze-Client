b65fafbad406ecbd6ff8cfc1e5b786a67bd578da
David Shack <david@tritonwebdev.com>
2021-01-08 08:31:59 -0600

    feat: add generic FormField component to wrap around form fields

---
+++ src/components/forms/FormField.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="my-5">
+    <label class="block text-sm leading-5 font-medium text-gray-700"
+      ><slot name="label"
+    /></label>
+    <div class="mt-1 relative rounded-md shadow-sm">
+      <slot />
+    </div>
+  </div>
+</template>
