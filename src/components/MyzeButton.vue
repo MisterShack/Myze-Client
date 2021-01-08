d9783d61049d343e1224d6ed6afdae7186a4f91e
David Shack <david@tritonwebdev.com>
2021-01-08 08:31:33 -0600

    feat: added MyzeButton component as a dynamic base component for buttons

---
+++ src/components/MyzeButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <button :class="state.classes">
+    <slot />
+  </button>
+</template>
+
+<script>
+  import { onMounted, reactive } from "vue";
+  export default {
+    props: {
+      theme: String,
+    },
+    setup(props) {
+      const state = reactive({
+        classes:
+          "shadow-sm rounded border px-5 py-2 text-base cursor-pointer font-semibold transition duration-150 ease-in",
+      });
+
+      onMounted(() => {
+        switch (props.theme) {
+          case "Light":
+            state.classes += "hover:border-gray-600 bg-white border-gray-400";
+            break;
+          case "Success":
+            state.classes +=
+              "hover:border-green-600 bg-green-200 text-green-800 border-green-400";
+            break;
+          case "Danger":
+            state.classes +=
+              "hover:red-900 bg-red-300 text-red-900 border-red-700";
+            break;
+        }
+      });
+
+      return {
+        state,
+      };
+    },
+  };
+</script>
