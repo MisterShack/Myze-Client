188e89f000af48c8040aa43e4f0ded81576f50e4
David Shack <david@tritonwebdev.com>
2021-01-16 17:09:51 -0600

    feat: added Layouts

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
 <template>
-  <router-view />
+  <component :is="layout">
+    <router-view />
+  </component>
 </template>
+
+<script>
+  import HomeLayout from "@/layouts/HomeLayout.vue";
+  import AppLayout from "@/layouts/AppLayout.vue";
+
+  import { useRoute } from "vue-router";
+  import { computed } from "vue";
+
+  export default {
+    components: { HomeLayout, AppLayout },
+    setup() {
+      const route = useRoute();
+
+      const layout = computed(() => route.meta.layout || AppLayout);
+
+      return {
+        layout,
+      };
+    },
+  };
+</script>
