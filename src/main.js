44985d2d3bc96b6cf5ce285fed2d2d53e74089fa
David <david@tritonwebdev.com>
2021-01-31 20:55:27 -0600

    refactor

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 import "./assets/index.css";
+import VueAxios from "vue-axios";
+import axios from "axios";
 
 createApp(App)
-  .use(store)
   .use(router)
+  .use(VueAxios, axios)
   .mount("#app");
