5be626fd119cc5a80ee8b747ad69256ccfdf743d
David <david@tritonwebdev.com>
2021-05-27 19:07:11 -0500

    Updated to provide bottom padding for the navbar

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,17 +1,16 @@
 <template>
   <main class="min-h-screen max-w-7xl">
-    <div class="pt-10 px-10 md:pl-28">
-      <Sidebar v-show="user" class="hidden md:block" />
+    <div class="pt-10 px-4 pb-12 md:px-10 md:pl-28">
+      <Sidebar v-show="user" />
       <div>
         <slot />
       </div>
     </div>
-    <BottomNavbar v-show="user" class="md:hidden" />
   </main>
 </template>
 
 <script>
-  import { computed, watch } from "vue";
+  import { computed } from "vue";
   import Sidebar from "@/components/Sidebar.vue";
   import BottomNavbar from "@/components/BottomNavbar.vue";
   import { realm } from "@/realm";
