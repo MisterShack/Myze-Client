83a8e76f472d926431091b7893a82c83321284f9
David <davidshack97@gmail.com>
2021-11-17 22:35:59 -0600

    Add transaction options and account types

--- src/helpers/Constants.ts
+++ src/helpers/Constants.ts
@@ -5,3 +5,18 @@ export const navLinks = {
   analytics: "Analytics",
   settings: "Settings",
 };
+
+export const transactionOptions = {
+  DEBIT: "Expense",
+  CREDIT: "Income",
+};
+
+export const accountTypes = {
+  CHEQUING: "Chequing",
+  CREDIT_CARD: "Credit Card",
+  SAVINGS: "Savings",
+  TFSA: "Tax-Free Savings",
+  LOAN: "Loan",
+  LOC: "Line of Credit",
+  RRSP: "Retirement Savings Plan",
+};
