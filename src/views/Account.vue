0cf32ca7dd680e2a28c4b3e97f97eaec1aaac510
David <davidshack97@gmail.com>
2021-11-20 00:25:07 -0600

    Refactor for Supabase / Store migration

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,16 +1,16 @@
 <template>
-  <div v-if="state.loading">
+  <div v-if="loading">
     <p>Loading...</p>
   </div>
   <template v-else>
     <div class="flex flex-wrap justify-centeritems-center md:justify-between">
       <div class="text-center mb-5 w-full md:text-left md:mb-0 md:w-auto">
-        <h1 class="font-thin text-lg">{{ state.account.name }}</h1>
+        <h1 class="font-thin text-lg">{{ account.name }}</h1>
         <h3 class="text-4xl text-light-blue-700 md:text-3xl">
           {{
             new Currency(
-              state.account.current_balance /
-                (state.account.type === "CREDIT_CARD" ? -100 : 100)
+              account.current_balance /
+                (account.type === "CREDIT_CARD" ? -100 : 100)
             ).format()
           }}
         </h3>
@@ -26,8 +26,7 @@
         <h3 class="text-4xl text-light-blue-700 md:text-3xl">
           {{
             new Currency(
-              futureBalance /
-                (state.account.type === "CREDIT_CARD" ? -100 : 100)
+              futureBalance / (account.type === "CREDIT_CARD" ? -100 : 100)
             ).format()
           }}
         </h3>
@@ -37,79 +36,84 @@
     <div class="flex space-x-4 items-center my-10">
       <template v-for="(label, key) in navLinks" :key="key">
         <a
-          v-if="key === state.activeNavigation"
+          v-if="key === activeNavigation"
           class="text-sm py-2  px-3 text-white bg-light-blue-700 rounded-md"
           >{{ label }}</a
         >
         <a
           v-else
           class="text-sm p-2 px-3 cursor-pointer opacity-60 hover:opacity-100"
-          @click="state.activeNavigation = key"
+          @click="activeNavigation = key"
           >{{ label }}</a
         >
       </template>
     </div>
 
-    <template v-if="!state.loading">
-      <Overview
-        v-if="state.activeNavigation === 'overview'"
-        :account="state.account"
-      />
+    <template v-if="!loading">
       <Transactions
-        v-if="state.activeNavigation === 'transactions'"
-        :account="state.account"
-        :vendors="state.vendors"
+        v-if="activeNavigation === 'transactions'"
+        :accountId="account.id"
       />
-      <Recurring
-        v-if="state.activeNavigation === 'recurring'"
-        :account="state.account"
-        :vendors="state.vendors"
+      <Settings
+        v-if="activeNavigation === 'settings'"
+        :accountId="account.id"
       />
 
-      <Settings
-        v-if="state.activeNavigation === 'settings'"
-        :account="state.account"
+      <Recurring
+        v-if="activeNavigation === 'recurring'"
+        :accountId="account.id"
       />
+      <!-- <Overview v-if="activeNavigation === 'overview'" :account="account" /> -->
     </template>
   </template>
 </template>
 
 <script>
-  import { computed, reactive, onMounted, onUnmounted } from "vue";
+  import { computed, reactive, ref } from "vue";
   import { useRoute } from "vue-router";
-  import { accountStore } from "@/store/account-store.ts";
-  import RecurringService from "@/services/RecurringService.ts";
+  import { store } from "@/store";
   import { navLinks } from "@/helpers/Constants";
+
+  import RecurringService from "@/services/RecurringService.js";
   import dayjs from "dayjs";
 
-  import Overview from "@/components/account/Overview.vue";
+  // import Overview from "@/components/account/Overview.vue";
   import Transactions from "@/components/account/Transactions.vue";
   import Recurring from "@/components/account/Recurring.vue";
   import Settings from "@/components/account/Settings.vue";
   import Currency from "@/helpers/Currency";
 
   export default {
-    components: { Overview, Transactions, Recurring, Settings },
+    // components: { Overview, Transactions, Recurring, Settings },
+    components: { Transactions, Settings, Recurring },
 
     setup() {
       let route = useRoute();
 
-      const state = reactive({
-        loading: true,
-        activeNavigation: "overview",
-        account: {},
-        vendors: {},
-        notifications: [],
-        futureTransactions: {},
+      const loading = computed(() => {
+        return store.loading;
       });
 
+      const activeNavigation = ref("overview");
 
+      const account = computed(() => store.accounts[route.params.id]);
 
+      const vendors = computed(() => store.vendors);
 
+      const futureTransactions = reactive({});
+
+      // futureTransactions = RecurringService.generateFutureTransactions(
+      //   account.id,
+      //   dayjs()
+      //     .add(4, "week")
+      //     .format("YYYY-MM-DD")
+      // );
+
+      // Generate future transactions from recurring service
       const futureBalance = computed(() => {
-        let futureBalance = state.account.current_balance;
+        let futureBalance = account.value.current_balance;
 
-        Object.values(state.futureTransactions).forEach((transactions) => {
+        Object.values(futureTransactions).forEach((transactions) => {
           transactions.forEach(
             (t) => (futureBalance += t.amount * (t.type === "DEBIT" ? -1 : 1))
           );
@@ -118,20 +122,12 @@
         return futureBalance;
       });
 
-      accountStore.loadAccounts().then(() => {
-        state.account = accountStore.getAccount(route.params.id);
-        state.vendors = accountStore.state.vendors;
-        state.futureTransactions = RecurringService.generateFutureTransactions(
-          state.account._id.toString(),
-          dayjs()
-            .add(4, "week")
-            .format("YYYY-MM-DD")
-        );
-        state.loading = false;
-      });
-
       return {
-        state,
+        loading,
+        activeNavigation,
+        account,
+        vendors,
+        futureTransactions,
         navLinks,
         Currency,
         dayjs,
