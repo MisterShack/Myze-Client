6fab9dd45a41b6591d57a6249c168cecc5b31ef4
David <davidshack97@gmail.com>
2022-01-05 08:53:45 -0600

    Refactored to have script tag on top

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,10 +1,10 @@
+<script lang="ts">
+  import { defineComponent } from "vue";
+  export default defineComponent({});
+</script>
+
 <template>
   <p class="text-4xl text-gray-800 font-medium">
     <slot />
   </p>
 </template>
-
-<script lang="ts">
-  import { defineComponent } from "vue";
-  export default defineComponent({});
-</script>
